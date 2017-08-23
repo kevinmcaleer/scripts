ed8b30e68ef1a0d5b80f8eab0bc1f4cfd4685772
Kevin McAleer <kevinmcaleer@me.com>
2017-08-23 15:17:36 +0100

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
 clear
 echo *** INSTALLING NODEJS VERSION 4.2.1 ***
-echo.
+echo
 wget https://nodejs.org/dist/v4.2.1/node-v4.2.1-linux-armv6l.tar.xz
 tar xJvf node-v4.2.1-linux-armv6l.tar.xz
-echo.
+echo
+echo *** COPYING FILES TO LOCAL FOLDER ***
+cd node-v4.2.1-linux-armv6l
+cp -R * /usr/local/
+export path=$PATH:/usr/local/bin
+echo
+echo *** CHECKING VERSION *** 
+echo
+node -v 
+npm -v
+git clone https://github.com/KhaosT/HAP-NodeJS.git
+cd HAP-NodeJS/
+
+npm install node-persist && npm install srp && npm install mdns
+npm install ed25519 && npm install curve25519 && npm install debug
+node BridgedCore.js
