51f30816e974ec710d47ff779918fc2e09ef6c09
Kevin McAleer <kevinmcaleer@me.com>
2017-08-23 15:26:54 +0100

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -4,6 +4,8 @@ echo "*** INSTALLING NODEJS VERSION 4.2.1 ***"
 echo "    -------------------------------"
 echo ""
 echo "Script by Kevin McAleer Copyright 2017"
+echo "--------------------------------------"
+echo ""
 wget https://nodejs.org/dist/v4.2.1/node-v4.2.1-linux-armv6l.tar.xz
 tar xJvf node-v4.2.1-linux-armv6l.tar.xz
 echo ""
