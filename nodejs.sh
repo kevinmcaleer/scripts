263d58f4bc594a49a3058cdd49f6e7ef6beed8b3
Kevin McAleer <kevinmcaleer@me.com>
2017-08-24 15:15:54 +0100

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -6,7 +6,7 @@ echo ""
 echo "Script by Kevin McAleer Copyright 2017"
 echo "--------------------------------------"
 echo ""
-wget https://nodejs.org/dist/v4.2.1/node-v4.4.4-linux-armv6l.tar.xz
+wget https://nodejs.org/dist/v4.4.4/node-v4.4.4-linux-armv6l.tar.xz
 tar xJvf node-v4.4.4-linux-armv6l.tar.xz
 echo ""
 echo "*** COPYING FILES TO LOCAL FOLDER ***"
