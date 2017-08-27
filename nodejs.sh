c7687a040a9826d96db7e5a8ae2738b2115b56db
Kevin McAleer <kevinmcaleer@me.com>
2017-08-27 16:58:18 +0100

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -8,6 +8,7 @@ echo "--------------------------------------"
 echo ""
 echo "*** 1/5 DOWNLOADING NODEJS ***"
 rm node-v4.4.4-linux-armv6l.tar.xz
+rm -R node-v4.4.4-linux-armv6l
 wget https://nodejs.org/dist/v4.4.4/node-v4.4.4-linux-armv6l.tar.xz
 tar xJvf node-v4.4.4-linux-armv6l.tar.xz
 echo ""
